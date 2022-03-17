72b477550a9caceee815e8a8e6e6f79014991b71
admin <admin@CHETUIWK0393>
2022-03-17 14:54:22 +0530

    Add project files.

---
+++ Suraj3/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Suraj3.Db;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Suraj3.Controllers
+{
+    public class HomeController : Controller
+    {
+        EmployeeEntities Db1 = new EmployeeEntities();
+        public ActionResult Index()
+        {
+            var data = Db1.Students.ToList();
+            return View(data);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Student e)
+        {
+            Db1.Students.Add(e);
+            Db1.SaveChanges();
+            return RedirectToAction("Index");
+            return View(e);
+        }
+    }
+    
+}
---
+++ Suraj3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Suraj3.MvcApplication" Language="C#" %>
